84be6eb9bd455d6d2aa8c7e4b0a69b7100bd6079
Davenchy <firon1222@gmail.com>
2019-08-20 21:14:27 +0200

    update backend torrent trackers list

--- api/torrents.js
+++ api/torrents.js
@@ -3,17 +3,7 @@ const pump = require("pump");
 const rangeParser = require("range-parser");
 const mime = require("mime");
 const client = new WebTorrent();
-const trackers = `udp://open.demonii.com:1337/announce
-udp://tracker.openbittorrent.com:80
-udp://tracker.coppersurfer.tk:6969
-udp://glotorrents.pw:6969/announce
-udp://tracker.opentrackr.org:1337/announce
-udp://torrent.gresille.org:80/announce
-udp://p4p.arenabg.com:1337
-udp://tracker.leechers-paradise.org:6969`
-  .split("\n")
-  .map(a => a.trim())
-  .filter(a => a);
+const trackers = require("./torrent-trackers");
 
 const toJSONInit = (torrent, cb) => {
   torrent.toJson = () => ({
