3d47da185995cba0401d0c0437f4b30477996a97
Davenchy <firon1222@gmail.com>
2019-09-06 19:17:23 +0200

    updated development env

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,9 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
 module.exports = {
+  configureWebpack: {
+    devtool: "source-map"
+  },
   devServer: {
     proxy: {
       "^/api": {
