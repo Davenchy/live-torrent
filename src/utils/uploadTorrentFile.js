260eccebc7170d236290673282385134abf14e50
Davenchy <firon1222@gmail.com>
2019-09-06 18:00:18 +0200

    Cleaned upload torrent file script

--- src/utils/uploadTorrentFile.js
+++ src/utils/uploadTorrentFile.js
@@ -1,5 +1,4 @@
 import Swal from "./sweetalert2";
-import WebTorrent from "webtorrent";
 import ParseTorrent from "parse-torrent";
 
 class TorrentFileUploader {
@@ -37,26 +36,11 @@ class TorrentFileUploader {
         "The selected file is not a torrent file",
         "error"
       );
-    this.readTorrentFile2(files[0]);
+    this.readTorrentFile(files[0]);
     this.clean();
   }
 
   readTorrentFile(file) {
-    const client = new WebTorrent();
-
-    client.on("error", err => {
-      console.error(err);
-      Swal.fire("Upload Failed!", err.message, "error");
-    });
-
-    client.add(file, torrent => {
-      torrent.destroy();
-      client.destroy();
-      this.onupload(torrent.infoHash);
-    });
-  }
-
-  readTorrentFile2(file) {
     ParseTorrent.remote(file, (err, torrent) => {
       if (err) {
         console.error(err);
